da58b05c854d694419a0e95421e1713c9b1110b9
June Park <junepark202012@gmail.com>
2024-04-07 18:32:29 +0900

    feat: add import.meta.env log in middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,8 @@ import {defineMiddleware} from "astro/middleware";
 
 export const onRequest = defineMiddleware((context, next) => {
     console.log("Middleware: onRequest")
-    console.log("import.meta.env", import.meta.env.VAR)
+    console.log("import.meta.env.VAR", import.meta.env.VAR)
+    console.log("import.meta.env", import.meta.env)
     console.log("context.locals.runtime.env.VAR", context.locals.runtime?.env.VAR)
     context.locals.cfVar = context.locals.runtime?.env.VAR
     return next();
